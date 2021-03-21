1f13071b050a7ef00fc8355009e8a526016b63ef
Mitiku Geleta <mitiku.geleta@tuni.fi>
2021-03-21 14:18:36 +0200

    -add morgage manager

---
+++ src/Mortgage.Calculator/Interfaces/IMortgageManager.cs
@@ -0,0 +1,7 @@
+﻿namespace Mortgage.Calculator.Interfaces
+{
+    public interface IMortgageManager
+    {
+        public string MortgageCalculatorResponse(string customerName, decimal amount, decimal apr, int loanTerm);
+    }
+}
---
+++ src/Mortgage.Calculator/MortgageManager.cs
@@ -0,0 +1,17 @@
+﻿using Mortgage.Calculator.Interfaces;
+
+namespace Mortgage.Calculator
+{
+    public class MortgageManager : IMortgageManager
+    {
+        public string MortgageCalculatorResponse(string customerName, decimal amount, decimal apr, int loanTerm)
+        {
+            var monthlyPaymenet = MortgageMath.FixedMonthlyPayment(amount, apr, loanTerm);
+
+            var response = "****************************************************************************************************\n" +
+                           $"Prospect 1: {customerName} wants to borrow {amount}€ for a period of {loanTerm} years and pay {monthlyPaymenet}€ each month \n" +
+                           "* **************************************************************************************************";
+            return response;                   
+        }
+    }
+}
